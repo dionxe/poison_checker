78fdb20e66426a66aaa52dc8deefa5c0c40602db
Dion <dion@xe.is>
2020-02-08 15:40:42 +0100

    remember selection poison checker form

--- app/controllers/poison_controller.rb
+++ app/controllers/poison_controller.rb
@@ -1,10 +1,10 @@
 class PoisonController < ApplicationController
 
   def index
-    criteria = params
+    @criteria = params
                  .permit(Mushroom.filters)
                  .select { |k,value| value.empty? == false }
-    mushrooms = Mushroom.where(criteria)
+    mushrooms = Mushroom.where(@criteria)
 
     @poison = PoisonProbability.calculate(mushrooms)
     @filters = Mushroom.filter_options
